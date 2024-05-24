8fa8ed72203f4df3a37634f62399605bece20412
Lasith123456 <lasith123456@gmail.com>
2024-05-24 13:20:56 +0530

    Initial commit

---
+++ src/main/java/com/devCom/web_app_posts_api/WebAppPostsApiApplication.java
@@ -0,0 +1,13 @@
+package com.devCom.web_app_posts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppPostsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppPostsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devCom/web_app_posts_api/WebAppPostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devCom.web_app_posts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppPostsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
