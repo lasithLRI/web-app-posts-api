d68a566c095623cd605f3b572faac1324e501fdf
Lasith123456 <lasith123456@gmail.com>
2024-05-25 22:51:03 +0530

    AddComment Feature added

--- src/main/java/com/devCom/web_app_posts_api/controller/PostController.java
+++ src/main/java/com/devCom/web_app_posts_api/controller/PostController.java
@@ -1,15 +1,13 @@
 package com.devCom.web_app_posts_api.controller;
 
+import com.devCom.web_app_posts_api.dto.request.RequestFeedBackDto;
 import com.devCom.web_app_posts_api.dto.request.RequestPostDto;
 import com.devCom.web_app_posts_api.service.PostService;
 import com.devCom.web_app_posts_api.util.StandardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/posts")
@@ -27,5 +25,21 @@ public class PostController {
                 new StandardResponse(201,"Post was Saved",postDto.getTitle()),
                 HttpStatus.CREATED
         );
+    }
+
+    @PutMapping("/comment{id}")
+    private ResponseEntity<StandardResponse> updateComments(
+
+            @PathVariable String id,
+            @RequestBody RequestFeedBackDto fbDto
+    ){
+
+        postService.addComment(fbDto,id);
+
+        return new ResponseEntity<>(
+                new StandardResponse(200,"Comment was added",fbDto.getName()),
+                HttpStatus.CREATED
+        );
+
     }
 }
